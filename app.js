7f389a98d26b5c612d1673434d9b90276ad3abba
UditDuhan <uditduhan23@gmail.com>
2020-09-09 19:41:10 +0530

    RESTful Blog Website created

---
+++ app.js
@@ -0,0 +1,129 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const expressSanitizer = require("express-sanitizer");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
+
+mongoose.connect("mongodb://localhost:27017/blog-app", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+    .then(() => console.log('Connected to DB!'))
+    .catch(error => console.log(error.message));
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+// Blog.create({
+//     title: "Dog Post",
+//     image: "https://images.unsplash.com/photo-1535930891776-0c2dfb7fda1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+//     body: "This is a blog post on dog."
+// });
+
+app.get("/", (req, res) => {
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", (req, res) => {
+    Blog.find({}, (err, blogs) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", {
+                blogs: blogs
+            });
+        }
+    });
+});
+
+app.get("/blogs/new", (req, res) => {
+    res.render("new");
+});
+
+app.post("/blogs", (req, res) => {
+    // var title = req.body.blog.title;
+    // var image = req.body.blog.image;
+    // var body = req.body.blog.body;
+    // var blog = {
+    //     title: title,
+    //     image: image,
+    //     body: body
+    // };
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.create(req.body.blog, (err, newlyCreated) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(newlyCreated);
+            res.redirect("/blogs");
+        }
+    });
+});
+
+app.get("/blogs/:id", (req, res) => {
+    Blog.findById(req.params.id, (err, foundBlog) => {
+        if (err) {
+            req.redirect("/blogs");
+        } else {
+            res.render("show", {
+                blog: foundBlog
+            });
+        }
+    });
+});
+
+app.get("/blogs/:id/edit", (req, res) => {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.findById(req.params.id, (err, foundBlog) => {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {
+                blog: foundBlog
+            });
+        }
+    });
+});
+
+app.put("/blogs/:id", (req, res) => {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
+app.delete("/blogs/:id", (req, res) => {
+    Blog.findByIdAndRemove(req.params.id, (err) => {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+app.listen("3000", () => {
+    console.log("Blog App Server has started.");
+});
